2d83691269e5ea20f8323e59d40996664c6865ce
jfleit <jfleit18@gmail.com>
2018-09-12 02:07:39 -0700

    Added youtube api and refined some code for testing purposes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import discord
 import error_handler
 from discord.ext import commands
 import config
 from elasticsearch import Elasticsearch
 
-bot = commands.Bot(command_prefix=['?', '!'], description="jake and amir bot!")
+bot = commands.Bot(command_prefix=['?', '!', '/'], description="jake and amir bot!")
 bot.add_cog(error_handler.CommandErrorHandler(bot))
 es = Elasticsearch()
+
 
 @bot.event
 async def on_ready():
@@ -15,16 +15,16 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command()
-async def ping():
-    await bot.say("pong!")
 
 @bot.command()
-async def search(*, search_str):
-    await bot.say("you searched " + search_str)
+async def search(*, quote):
     search_res = es.search(index="ja-test_index",
-                           body={"query": {"match": {"script": {"query": search_str, "fuzziness": "AUTO"}}}})
-    for hit in search_res['hits']['hits']:
-        await bot.say(hit['_source']['link'])
+                           body={"query": {"match": {"script": {"query": quote, "fuzziness": "AUTO"}}}})
+    elasticsearch_hits = search_res['hits']['hits']
 
-bot.run(config.token)
+    if elasticsearch_hits:
+        await bot.say(elasticsearch_hits[0]['_source']['link'])
+    else:
+        await bot.say("Sorry, couldn't find an episode for that quote.")
+
+bot.run(config.discord_token)
--- maintest.py
+++ maintest.py
@@ -1,7 +1,26 @@
 from elasticsearch import Elasticsearch
 import json
+import config
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 es = Elasticsearch()
+current_index = "ja-test_index"
+
+GOOGLE_API_KEY = config.google_api_key
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+
+def youtube_search(query):
+    try:
+        youtube = build(serviceName=YOUTUBE_API_SERVICE_NAME, version=YOUTUBE_API_VERSION, developerKey=GOOGLE_API_KEY)
+        search_response = youtube.search().list(q=query, type="video", maxResults=1, part="id").execute()
+
+        return "https://www.youtube.com/watch?v=" + search_response['items'][0]['id']['videoId']
+
+    except HttpError as e:
+        print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
 
 
 def create_script(script_file, link):
@@ -14,24 +33,24 @@ def create_script(script_file, link):
 
 
 def save_script(doc):
-    return es.index(index="ja-test_index", doc_type="script", body=doc)
+    return es.index(index=current_index, doc_type="script", body=doc)
 
 
 def search_script(query):
     # search for a script + link
-    search_res = es.search(index="ja-test_index", body={"query": {"match": {"script": {"query": query}}}})
+    search_res = es.search(index=current_index, body={"query": {"match": {"script": {"query": query}}}})
     for hit in search_res['hits']['hits']:
         print(hit)
 
 
-def main():
-    # triathlon = create_script("triathlon.txt", "https://www.youtube.com/watch?v=INVdbXTuPVI")
-    # save_script(triathlon)
-    # corduroy = create_script("corduroy_pants.txt", "https://www.youtube.com/watch?v=ao2xLlxiaNo")
-    # save_script(corduroy)
-
-    search_script("if you're the trunk, then you got punked")
+def match_all_scripts():
+    search_res = es.search(index=current_index, body={"query": {"match_all": {}}})
+    for hit in search_res['hits']['hits']:
+        print(hit)
 
 
-if __name__ == "__main__":
-    main()
+def clear_index():
+    es.indices.delete(index='ja-test_index', ignore=[400, 404])
+
+# save_script(create_script("triathlon.txt", youtube_search("Jake and Amir Triathlon")))
+# save_script(create_script("corduroy_pants.txt", youtube_search("Jake and Amir Corduroy Pants")))
